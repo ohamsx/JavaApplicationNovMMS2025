9d97142507204c97450224669a6c6df1a851a4e0
ohamsx <ohamsniit@gmail.com>
2026-06-03 10:14:07 +0100

    Java Output

---
+++ Mercyben.java
@@ -0,0 +1,5 @@
+public class Mercyben{
+	public static void main(String[] args){
+		System.out.println("Hello ma, how are you doing?");
+	}
+}	
---
+++ Output.java
@@ -0,0 +1,13 @@
+public class Output{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, this is my first java code");
+		System.out.println("Learning java is fun");
+		System.out.print("Here i am using the print method.");
+		System.out.print("This text will be appended ");
+		System.out.printf("%nMercy Ben is %d years old.%n",25);
+		System.out.printf("My account balance is %c%,.2f%n",'$',5000000.243995);
+		System.out.printf("%s is a boy %n","Benjamin");
+		System.out.printf("Do you love java? %b%n", true);
+		System.out.printf("Fuel price in Nigeria has been increased by %%80");
+	}
+}	
